69271922653b5c96d32931d4a14b854dba7405dc
sbkbrah <k.stannard@live.com>
2020-03-10 22:08:37 -0500

    Initial Commit

---
+++ PlateMathV3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlateMathV3
+//
+//  Created by Koty Stannard on 3/10/20.
+//  Copyright © 2020 Koty Stannard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
